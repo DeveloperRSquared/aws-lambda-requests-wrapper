460135594dc750b44e66c02bbcf1c8c3c5e61df7
Rikhil <23627977+rikhilrai@users.noreply.github.com>
2022-01-28 13:05:53 +0000

    .

--- src/aws_lambda_requests_wrapper/__init__.py
+++ src/aws_lambda_requests_wrapper/__init__.py
@@ -1,6 +1,7 @@
+# type: ignore
 try:
     from importlib.metadata import version
 except ImportError:
-    from importlib_metadata import version  # type: ignore[import,no-redef]
+    from importlib_metadata import version
 
 __version__: str = version(__name__)
